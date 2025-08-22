08a894c61b5c9618ce9965062a54f14e9564dae3
Federico Verly <federicoverly@gmail.com>
2025-08-22 04:29:26 +0200

    Add filter

--- src/BoyfriendInspiration/BoyfriendInspiration.tsx
+++ src/BoyfriendInspiration/BoyfriendInspiration.tsx
@@ -2,10 +2,32 @@ import styles from "./BoyfriendInspiration.module.css";
 import songInspiration from "./songInspiration.json";
 import { BackHomeButton } from "../BackHomeButton/BackHomeButton";
 import { BoyfriendCard } from "../BoyfriendCard/BoyfriendCard";
-
-console.log(songInspiration);
+import { useCallback, useMemo, useState } from "react";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  InputLabel,
+  SelectChangeEvent,
+} from "@mui/material";
 
 export const BoyfriendInspiration = () => {
+  const [boyfriend, setBoyfriend] = useState<string>("");
+
+  const boyfriendNames = useMemo(() => {
+    return Array.from(new Set(songInspiration.map((song) => song.boyfriend)));
+  }, []);
+
+  const handleBoyfriendChange = useCallback((event: SelectChangeEvent) => {
+    console.log(event);
+    setBoyfriend(event.target.value as string);
+  }, []);
+
+  const filteredSongs = useMemo(() => {
+    if (boyfriend === "" || !boyfriend) return songInspiration;
+    return songInspiration.filter((song) => song.boyfriend === boyfriend);
+  }, [boyfriend]);
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -14,7 +36,26 @@ export const BoyfriendInspiration = () => {
         Unsure? Check it out!
       </div>
 
-      {songInspiration.map((song) => (
+      <div className={styles.filterTitle}>Filter by boyfriend!</div>
+      <FormControl>
+        <InputLabel id="boyfriend-select-label">Boyfriend</InputLabel>
+
+        <Select
+          id="boyfriend-select-label"
+          className={styles.select}
+          value={boyfriend}
+          onChange={handleBoyfriendChange}
+          placeholder="Select a boyfriend"
+          label="Boyfriend"
+        >
+          {boyfriendNames.map((name, index) => (
+            <MenuItem key={index} value={name}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {filteredSongs.map((song) => (
         <BoyfriendCard boyfriendInspiration={song} />
       ))}
       <BackHomeButton />
